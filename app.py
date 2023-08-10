9d07cfad6028adec42671e4f1f7e3d43f80a73f1
Michael Rodriguez <est.michael.reyes@unimilitar.edu.co>
2023-08-10 09:22:14 -0500

    gh

--- app.py
+++ app.py
@@ -1,62 +1,42 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import numpy as np
-import sounddevice as sd
-import soundfile as sf
 from tensorflow import keras
 import librosa
+import traceback
+import audioread.exceptions
 
-# Cargar el modelo entrenado
 model = keras.models.load_model('final_model.h5')
-
-# Definir las emociones disponibles
 emotions = ['Agradecimiento', 'Ansiedad', 'Curiosidad', 'Expectativa', 'Felicidad', 'Seguridad', 'Tranquilidad']
 
-# Función para preprocesar el audio
-def preprocess_audio(audio_path):
-    # Cargar el archivo de audio
-    audio, sr = librosa.load(audio_path, sr=None)
-    
-    # Normalizar la amplitud del audio
-    audio /= np.max(np.abs(audio))
-    
-    # Extraer características de audio utilizando Mel-Frequency Cepstral Coefficients (MFCC)
+def preprocess_audio(audio):
+    sr = 22050  # Use the same sampling rate used during recording (you can adjust this accordingly)
+    audio = np.array(audio) / np.max(np.abs(audio))
     mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=13)
-    
-    # Ajustar la longitud de MFCC a 100 frames
     if mfcc.shape[1] > 100:
         mfcc = mfcc[:, :100]
     else:
         mfcc = np.pad(mfcc, ((0, 0), (0, 100 - mfcc.shape[1])), mode='constant')
-    
-    # Redimensionar a 3D (1, características, frames, canales)
     mfcc = np.expand_dims(mfcc, axis=0)
     mfcc = np.expand_dims(mfcc, axis=-1)
-    
     return mfcc
 
-# Función para predecir la emoción
 def predict_emotion(mfcc):
-    # Predecir la emoción
     predictions = model.predict(mfcc)
-    
-    # Obtener la emoción con mayor probabilidad
     emotion_index = np.argmax(predictions)
     emotion = emotions[emotion_index]
-    
-    # Obtener la probabilidad de la emoción predicha
     probability = predictions[0][emotion_index]
-    
     return emotion, probability
 
-# Crear una aplicación Flask
 app = Flask(__name__)
 
-# Endpoint para cargar el archivo de audio y realizar la predicción
-@app.route('/predict_emotion', methods=['POST'])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict_emotion_endpoint', methods=['POST'])
 def predict_emotion_endpoint():
     try:
-        # Verificar si se recibe un archivo de audio
         if 'archivo_audio' not in request.files:
             return jsonify({'error': 'No se encontró el archivo de audio'}), 400
 
@@ -64,24 +44,26 @@ def predict_emotion_endpoint():
         if archivo.filename == '':
             return jsonify({'error': 'No se seleccionó ningún archivo'}), 400
 
-        # Guardar el archivo de audio en un directorio temporal
-        audio_path = 'temp_audio.wav'
-        archivo.save(audio_path)
+        audio = np.frombuffer(archivo.read(), dtype=np.int16)
+        mfcc = preprocess_audio(audio)
 
-        # Preprocesar el audio
-        mfcc = preprocess_audio(audio_path)
-
-        # Realizar la predicción de la emoción
         emotion, probability = predict_emotion(mfcc)
 
-        # Eliminar el archivo de audio temporal
-        os.remove(audio_path)
+        # Establecemos el URL del GIF según la emoción detectada
+        switch_emotion = {
+            "Felicidad": "/static/emojis/felizAndres.gif",
+            "Ansiedad": "/static/emojis/ansiedadAndres.gif",
+            "Curiosidad": "/static/emojis/curiosidadAndres.gif",
+            "Tranquilidad": "/static/emojis/tranquilidadAndres.gif"
+        }
+        gif_url = switch_emotion.get(emotion, "/static/emojis/tranquilidadAndres.gif")
 
-        # Devolver los resultados
-        return jsonify({'emotion': emotion, 'probability': probability}), 200
-
+        return jsonify({'emotion': emotion, 'probability': probability, 'gif_url': gif_url}), 200
+    except audioread.exceptions.NoBackendError as e:
+        return jsonify({'error': 'No se pudo encontrar un backend para leer el archivo de audio.'}), 500
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        traceback.print_exc()
+        return jsonify({'error': 'Error en el procesamiento del audio.'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -1,23 +1,150 @@
-function processAudio() {
-    const audioFileInput = document.getElementById('audioFileInput');
-    const resultsDiv = document.getElementById('results');
-    const emotionResult = document.getElementById('emotionResult');
-    const probabilityResult = document.getElementById('probabilityResult');
+let mediaRecorder;
+let recordedChunks = [];
+let isRecording = false;
 
+function startRecording() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        alert('Tu navegador no soporta la API de Web Audio para grabación de audio.');
+        return;
+    }
+
+    const constraints = { audio: true };
+
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function(stream) {
+            mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.ondataavailable = function(event) {
+                if (event.data.size > 0) {
+                    recordedChunks.push(event.data);
+                }
+            };
+
+            mediaRecorder.onstop = function() {
+                const audioBlob = new Blob(recordedChunks, { type: 'audio/wav' });
+                const audioUrl = URL.createObjectURL(audioBlob);
+                processAudio(audioUrl);
+            };
+
+            mediaRecorder.start();
+            isRecording = true;
+            toggleRecordingButton();
+        })
+        .catch(function(err) {
+            console.error('Error al acceder al dispositivo de audio:', err);
+        });
+}
+
+function stopRecording() {
+    if (mediaRecorder && isRecording) {
+        mediaRecorder.stop();
+        isRecording = false;
+        toggleRecordingButton();
+    }
+}
+
+function toggleRecordingButton() {
+    const recordButton = document.getElementById('recordButton');
+    if (isRecording) {
+        recordButton.textContent = 'Detener Grabación';
+        recordButton.classList.add('recording');
+    } else {
+        recordButton.textContent = 'Iniciar Grabación';
+        recordButton.classList.remove('recording');
+    }
+}
+
+function processAudio(audioUrl) {
     const formData = new FormData();
-    formData.append('archivo_audio', audioFileInput.files[0]);
+    formData.append('archivo_audio', new Blob([recordedChunks[0]], { type: 'audio/wav' }));
 
-    fetch('/predict_emotion', {
+    fetch('/predict_emotion_endpoint', {
         method: 'POST',
         body: formData,
     })
     .then(response => response.json())
     .then(data => {
-        emotionResult.textContent = data.emotion;
-        probabilityResult.textContent = data.probability.toFixed(2);
-        resultsDiv.style.display = 'block';
+        showResults(data);
     })
     .catch(error => {
         console.error('Error:', error);
     });
 }
+
+function showResults(data) {
+    const resultsDiv = document.getElementById('results');
+    const emotionResult = document.getElementById('emotionResult');
+    const probabilityResult = document.getElementById('probabilityResult');
+    const gifImg = document.getElementById('gifImg'); 
+
+    if (data.emotion && data.probability) {
+        emotionResult.textContent = data.emotion;
+        probabilityResult.textContent = data.probability.toFixed(2);
+        resultsDiv.style.display = 'block';
+
+        gifImg.src = data.gif_url; 
+
+        const modal = document.getElementById('myModal');
+        modal.style.display = "block";
+    } else {
+        emotionResult.textContent = 'Error en la predicción.';
+        probabilityResult.textContent = '';
+        resultsDiv.style.display = 'block';
+    }
+}
+
+function showResults(data) {
+    const resultsDiv = document.getElementById('results');
+    const emotionResult = document.getElementById('emotionResult');
+    const probabilityResult = document.getElementById('probabilityResult');
+
+    if (data.emotion && data.probability) {
+        emotionResult.textContent = data.emotion;
+        probabilityResult.textContent = data.probability.toFixed(2); // Fixed to 2 decimal places
+        resultsDiv.style.display = 'block';
+
+        // Show the GIF based on the detected emotion
+        const gifImg = document.getElementById('gifImg');
+        gifImg.src = data.gif_url;
+    } else {
+        // Mostrar mensaje de error en lugar de resultados incompletos.
+        emotionResult.textContent = 'Error en la predicción.';
+        probabilityResult.textContent = '';
+        resultsDiv.style.display = 'block';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const recordButton = document.getElementById('recordButton');
+    recordButton.addEventListener('click', function() {
+        if (isRecording) {
+            stopRecording();
+        } else {
+            startRecording();
+        }
+    });
+});
+     
+
+document.addEventListener('DOMContentLoaded', function() {
+    const recordButton = document.getElementById('recordButton');
+    recordButton.addEventListener('click', function() {
+        if (isRecording) {
+            stopRecording();
+        } else {
+            startRecording();
+        }
+    });
+
+    const modal = document.getElementById('myModal');
+    const closeButton = document.getElementsByClassName("close")[0];
+
+    closeButton.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+});
